6a023d139b941d5029f7f45e475df40dfa5f570e
ADEL BOUATTA <83227627+adelwebdev@users.noreply.github.com>
2022-05-11 23:41:43 +0300

    Add files via upload

---
+++ src/components/connectModel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const connectModel = () => {
+    return (
+        <div>
+            <h1>CONNECT MODAL</h1>
+            
+            
+        </div>
+    );
+};
+
+export default connectModel;
