61824987f51ed81ab08dae355ca01fe8d8182532
ADEL BOUATTA <83227627+adelwebdev@users.noreply.github.com>
2022-05-14 23:46:49 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import connectModal from "./components/connectModal";
 
 function App() {
   return (
     <div>
       <div className='app-header'>
-
+      <connectModal/>
+       
+        
+        
+ 
       </div>
 
       <div className='posts-container'>
